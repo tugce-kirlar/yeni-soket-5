681a6eedfef46435f362315a79641b6b50e4fbd3
201410009@ohu.edu.tr <201410009@ohu.edu.tr>
2022-11-21 19:37:46 +0300

    Proje dosyası ekle.

---
+++ Client/Client/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txt_text = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_chat = new System.Windows.Forms.RichTextBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 275);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 18;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(250, 19);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 17;
+            this.btnConnect.Text = "Bağlan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "İsminiz : ";
+            // 
+            // txName
+            // 
+            this.txName.Location = new System.Drawing.Point(72, 22);
+            this.txName.Name = "txName";
+            this.txName.Size = new System.Drawing.Size(172, 20);
+            this.txName.TabIndex = 15;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(328, 15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(145, 211);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tüm Clientler";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(133, 186);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txt_text
+            // 
+            this.txt_text.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_text.Location = new System.Drawing.Point(13, 235);
+            this.txt_text.Name = "txt_text";
+            this.txt_text.Size = new System.Drawing.Size(178, 20);
+            this.txt_text.TabIndex = 14;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(197, 234);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(103, 23);
+            this.btnSend.TabIndex = 13;
+            this.btnSend.Text = "Gönder";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb_chat);
+            this.groupBox1.Location = new System.Drawing.Point(10, 48);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(312, 180);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gelen Mesaj :";
+            // 
+            // rb_chat
+            // 
+            this.rb_chat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rb_chat.Location = new System.Drawing.Point(3, 16);
+            this.rb_chat.Name = "rb_chat";
+            this.rb_chat.Size = new System.Drawing.Size(306, 161);
+            this.rb_chat.TabIndex = 1;
+            this.rb_chat.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 331);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txName);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txt_text);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txt_text;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rb_chat;
+    }
+}
+
---
+++ Client/Client/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        private Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //InterNetwork = ipv4 ailesi için
+        //SocketType.Dgram= UDP için
+        //SocketType.Stream= TCP için
+        //ProtocolType.IP = TCP ve UDP 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[] receivedBuf = new byte[1024];//veri almak için yer ayırdık
+        private void ReceiveData(IAsyncResult ar)//burası asenkron oldugu için hep çalışır thread gibi veriyi almak için
+        {
+
+            int listede_yok = 0;//yok
+            try
+            {
+
+                Socket socket = (Socket)ar.AsyncState;//asenkron soketi alırız
+                int received = socket.EndReceive(ar);//verinin toplam uzunlugu
+                byte[] dataBuf = new byte[received];//verıyı byte cevırdık
+                Array.Copy(receivedBuf, dataBuf, received);//dataBuf=receivedBuf ve received uzunluk
+                string gelen = Encoding.ASCII.GetString(dataBuf).ToString();//serverdan gelen mesaj
+                if (gelen.Contains("sil*"))
+                {
+                    string parcala = gelen.Substring(4, (gelen.Length - 4));
+                    Console.WriteLine("degerim  " + parcala);
+                    for (int j = 0; j < listBox1.Items.Count; j++)//list boxtanda kaldır
+                    {
+                        if (listBox1.Items[j].Equals(parcala))
+                        {
+                            listBox1.Items.RemoveAt(j);
+
+                        }
+                    }
+                }
+                else if (gelen.Contains("@"))//içerisinde @ içeriyorsa clienti listeye eklicez
+                {
+
+                    for (int i = 0; i < listBox1.Items.Count; i++)//listedeki itemler kadar dön
+                    {
+                        if (listBox1.Items[i].ToString().Equals(gelen))//listede varsa o client
+                        {
+                            listede_yok = 1;//var
+                        }
+                    }
+                    if (listede_yok == 0)//yoksa  ekle clienti
+                    {
+                        string ben = "@" + txName.Text;
+                        if (ben.Equals(gelen))//kendimi ekleme
+                        {
+
+                        }
+                        else
+                        {
+                            listBox1.Items.Add(gelen);
+                        }
+                    }
+
+                }
+                else
+                {
+                    //label3.Text = (gelen);
+                    rb_chat.AppendText(gelen + "\n");
+                }
+
+
+                //rb_chat.AppendText("\nServer: " + label3.Text);
+
+                _clientSocket.BeginReceive(receivedBuf, 0, receivedBuf.Length, SocketFlags.None, new AsyncCallback(ReceiveData), _clientSocket);
+                /*
+          buffer=Türünde bir dizi Byte yani alınan veri için depolama konumu.
+          offset=Sıfır tabanlı konumu buffer , alınan verileri depolamak parametre.
+          size=Almak için bayt sayısı.
+          socketFlags=Bit seviyesinde birleşimini SocketFlags değerleri.
+          callback=Bir AsyncCallback işlemi tamamlandığında harekete geçirmek için bir yönteme başvuran bir temsilci.
+          state=Alma işlemi hakkında bilgi içeren bir kullanıcı tanımlı nesne.Bu nesne için geçirilen EndReceive işlemi tamamlandığında temsilci.
+                https://msdn.microsoft.com/tr-tr/library/dxkwh6zw(v=vs.110).aspx adresine bakabilirsin
+                  */
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ReceiveData() metodunda hata " + e.Message);
+            }
+
+        }
+
+        private void SendLoop()
+        {
+            while (true)
+            {
+                //Console.WriteLine("Enter a request: ");
+                //string req = Console.ReadLine();
+                //byte[] buffer = Encoding.ASCII.GetBytes(req);
+                //_clientSocket.Send(buffer);
+
+                byte[] receivedBuf = new byte[1024];
+                int rev = _clientSocket.Receive(receivedBuf);
+                if (rev != 0)
+                {
+                    byte[] data = new byte[rev];
+                    Array.Copy(receivedBuf, data, rev);
+                    // label3.Text = ("Received: " + Encoding.ASCII.GetString(data));
+                    rb_chat.AppendText("\nServer: " + Encoding.ASCII.GetString(data) + "\n");
+                }
+                else _clientSocket.Close();
+
+            }
+        }
+
+        private void LoopConnect()
+        {
+            int attempts = 0;
+            while (!_clientSocket.Connected)//server çalışmıyorsa(çalışısaya kadar döngü döner)
+            {
+                try
+                {
+                    attempts++;
+                    _clientSocket.Connect("127.0.0.1", 100);//127.0.0.1=IPAddress.Loopback demek 100 portuna bağlan
+                }
+                catch (SocketException)
+                {
+                    //Console.Clear();
+                    //   label3.Text = ("bağlantılar: " + attempts.ToString());
+                    Console.WriteLine("bağlantılar: " + attempts.ToString());
+                }
+            }
+            // SendLoop();
+            _clientSocket.BeginReceive(receivedBuf, 0, receivedBuf.Length, SocketFlags.None, new AsyncCallback(ReceiveData), _clientSocket);//AsyncCallback thread gibi asenkron eş zamansız çalışıyor
+            byte[] buffer = Encoding.ASCII.GetBytes("@@" + txName.Text);//ismimizin başına 2 tane @@ koydum belli olsun
+            _clientSocket.Send(buffer);//veriyi gönderdim servera
+            label3.Text = ("servere bağlandı!");//servere bağlandı
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            Thread t1 = new Thread(LoopConnect);
+            t1.Start();
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+            if (_clientSocket.Connected)//client servera bağlı ise
+            {
+                string tmpStr = "";
+                foreach (var item in listBox1.SelectedItems)//listboxtaki seçili itemlere
+                {
+
+                    tmpStr = listBox1.GetItemText(item);//isimlerini
+                    byte[] buffer = Encoding.ASCII.GetBytes(tmpStr + " :" + txt_text.Text + "*" + txName.Text);//byte çevir
+                    _clientSocket.Send(buffer);//ve gönder ip ve porta
+                    Thread.Sleep(20);//yapmasanda olur(fakat 4 cliente bırden mesaj gonderınce dıgerlerine gıtmeyebılir)
+
+                }
+                if (tmpStr.Equals(""))
+                {
+                    MessageBox.Show("lütfen listeden değer seçiniz");
+                }
+                else
+                {
+                    rb_chat.AppendText(txName.Text + ": " + txt_text.Text + "\n");
+                }
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;//dinamik nesne eklemek için listeye
+        }
+    }
+}
